43bed83bc57ad8ab6c883a8d487cf9dd746ae661
John Brewer <gzroms@hotmail.com>
2014-12-03 05:52:11 -0500

    Update and rename slim.mk to validus.mk

--- validus.mk
+++ validus.mk
@@ -1,5 +1,5 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/slim/config/gsm.mk)
+$(call inherit-product, vendor/validus/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := shamu
@@ -9,18 +9,18 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 # Inherit some common Slim stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/validus/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/moto/shamu/aosp_shamu.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := shamu
-PRODUCT_NAME := slim_shamu
+PRODUCT_NAME := validus_shamu
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 6
-PRODUCT_MANUFACTURER := Moto
+PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RESTRICT_VENDOR_FILES := false
